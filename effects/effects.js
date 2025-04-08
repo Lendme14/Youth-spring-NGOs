aa415ddbf11e512b519d08c69b2860c7414a9b2d
Nwabueze Christian Izuchukwu <izuchukwujnwabueze@gmail.com>
2025-04-08 06:18:52 -0700

    Create effects.js

---
+++ effects/effects.js
@@ -0,0 +1,13 @@
+<script>
+    const neonBox = document.getElementById('neonBox');
+    const toggleBtn = document.getElementById('toggleBtn');
+
+    let isVisible = true;
+
+    toggleBtn.addEventListener('click', () => {
+      isVisible = !isVisible;
+      neonBox.classList.toggle('hidden');
+      toggleBtn.classList.toggle('hidden-btn');
+      toggleBtn.textContent = isVisible ? 'Hide' : 'Show';
+    });
+  </script>
